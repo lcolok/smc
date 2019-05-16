bce963037eec43410bda1eafdfc288460ad86435
lcolok(Home) <425311101@qq.com>
2019-05-17 01:33:15 +0800

    新增一系列GIF动图

--- src/components/core/Filter.vue
+++ src/components/core/Filter.vue
@@ -133,7 +133,15 @@ export default {
       'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-1.23832d31.jpg',
       'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg',
       'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-3.3a54f533.jpg',
-      'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-4.3b7e38ed.jpg'
+      'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-4.3b7e38ed.jpg',
+      'https://cdn.dribbble.com/users/2453998/screenshots/5895939/fruit_ninja.gif',
+      'https://cdn.dribbble.com/users/31664/screenshots/5826010/speed_kills_2.gif',
+      'https://cdn.dribbble.com/users/31664/screenshots/6324479/dribbble_1.gif',
+      'https://cdn.dribbble.com/users/634508/screenshots/6176810/longwindingroad_dribbble.gif',
+      'https://cdn.dribbble.com/users/1903950/screenshots/6120821/shot_3__1.gif',
+      'https://cdn.dribbble.com/users/1903950/screenshots/6071583/comp_4.gif',
+      'https://cdn.dribbble.com/users/1903950/screenshots/6046188/dribble__1_.gif',
+      'https://images-cdn.shimo.im/tncnW1p0rUsZA8PR/cat_washer_dribbble.gif'
     ]
   }),
 
