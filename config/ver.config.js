b5ae5f840d21f65e9e9daae2b1216cdc2f8c6ff1
lcolok(JUB) <425311101@qq.com>
2019-05-20 21:31:15 +0800

    把旧的rotate.js闲置放在垃圾桶

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 0,
-		minor: 60,
-		build: 46,
+		minor: 61,
+		build: 0,
 		revision: 0
 	},
-	whole: "0.60.46(198)",
-	commits: 198
+	whole: "0.61.0(199)",
+	commits: 199
 }
