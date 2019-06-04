86d3506bc2a95bcb0f8daf991f9fcc0a12a3b9b7
lcolok(Home) <425311101@qq.com>
2019-06-05 03:05:31 +0800

    更换侧栏动图

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 0,
-		minor: 89,
-		build: 16,
+		minor: 90,
+		build: 5,
 		revision: 0
 	},
-	whole: "0.89.16(276)",
-	commits: 276
+	whole: "0.90.5(277)",
+	commits: 277
 }
--- src/store/modules/app/state.js
+++ src/store/modules/app/state.js
@@ -1,26 +1,26 @@
 const images = [
-  'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-1.23832d31.jpg',
-  'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg',
-  'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-3.3a54f533.jpg',
-  'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-4.3b7e38ed.jpg',
-  'https://cdn.dribbble.com/users/2453998/screenshots/5895939/fruit_ninja.gif',
-  'https://cdn.dribbble.com/users/31664/screenshots/5826010/speed_kills_2.gif',
-  'https://cdn.dribbble.com/users/31664/screenshots/6324479/dribbble_1.gif',
-  'https://cdn.dribbble.com/users/634508/screenshots/6176810/longwindingroad_dribbble.gif',
-  'https://cdn.dribbble.com/users/1903950/screenshots/6120821/shot_3__1.gif',
-  'https://cdn.dribbble.com/users/1903950/screenshots/6071583/comp_4.gif',
-  'https://cdn.dribbble.com/users/1903950/screenshots/6046188/dribble__1_.gif',
-  'https://images-cdn.shimo.im/tncnW1p0rUsZA8PR/cat_washer_dribbble.gif'
+	'https://cdn.dribbble.com/users/59947/screenshots/6145574/run-for-dribbble.gif',
+	'https://cdn.dribbble.com/users/500242/screenshots/6357566/scrollboi_finalfinalpdf_render2_dribbble2.gif',
+	'https://cdn.dribbble.com/users/634508/screenshots/5755935/kittyyingyang_dribbble.gif',
+	'https://cdn.dribbble.com/users/330915/screenshots/6554409/1_new_condo.gif',
+	'https://cdn.dribbble.com/users/2453998/screenshots/5895939/fruit_ninja.gif',
+	'https://cdn.dribbble.com/users/31664/screenshots/5826010/speed_kills_2.gif',
+	'https://cdn.dribbble.com/users/31664/screenshots/6324479/dribbble_1.gif',
+	'https://cdn.dribbble.com/users/634508/screenshots/6176810/longwindingroad_dribbble.gif',
+	'https://cdn.dribbble.com/users/1903950/screenshots/6120821/shot_3__1.gif',
+	'https://cdn.dribbble.com/users/1903950/screenshots/6071583/comp_4.gif',
+	'https://cdn.dribbble.com/users/1903950/screenshots/6046188/dribble__1_.gif',
+	'https://images-cdn.shimo.im/tncnW1p0rUsZA8PR/cat_washer_dribbble.gif',
 ];
 
-const colors = ["primary", "info", "success", "warning", "danger",]
+const colors = ['primary', 'info', 'success', 'warning', 'danger'];
 
 export default {
-  devPort: null,
-  drawer: null,
-  color: 'primary',
-  colors: colors,
-  image: images[Math.floor(images.length * Math.random())],//随机显示某一张
-  images: images,
-  sidebarBackgroundColor: 'rgba(27, 27, 27, 0.74)'
-}
+	devPort: null,
+	drawer: null,
+	color: 'primary',
+	colors: colors,
+	image: images[Math.floor(images.length * Math.random())], //随机显示某一张
+	images: images,
+	sidebarBackgroundColor: 'rgba(27, 27, 27, 0.74)',
+};
