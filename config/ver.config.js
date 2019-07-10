c9f6910325a82c34ca8b1c83e68b2c424ed3ba08
lcolok(Home) <425311101@qq.com>
2019-07-11 03:01:38 +0800

    数据清洗干净

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 0,
-		minor: 151,
-		build: 0,
+		minor: 152,
+		build: 59,
 		revision: 0
 	},
-	whole: "0.151.0(342)",
-	commits: 342
+	whole: "0.152.59(343)",
+	commits: 343
 }
--- server/core/routes/custom/tools/get/u2a.js
+++ server/core/routes/custom/tools/get/u2a.js
@@ -1,22 +1,12 @@
 //uploaderURL to attachmentURL using the expand.js
 const AV = require('leanengine');
-
-let axios = require('axios');
-
-axios.interceptors.request.use(
-	function(request) {
-		request['headers']['common']['Accept'] = 'application/json;charset=gb2312;';
-		return request;
-	},
-	function(error) {
-		return Promise.reject(error);
-	},
-);
+const path = require('path');
+const expand = require(path.resolve('server/utils/expandUtils'));
 
 module.exports = (req, res, next) => {
 	const query = new AV.Query('ShimoBed');
 	query.doesNotExist('attachmentURL'); //空值查询
-	query.limit(500); //请求数量上限为1000条
+	query.limit(1000); //请求数量上限为1000条
 	query
 		.find()
 		.then(async every => {
@@ -24,34 +14,64 @@ module.exports = (req, res, next) => {
 			console.log('总数:' + len);
 
 			every
-				.filter(e => {
-					return !e.get('attachmentURL');
+				.filter(object => {
+					return !object.get('attachmentURL');
 				})
 				.map(async each => {
-					let attachmentURL, uploaderURL, expandedURL;
+					let attachmentURL,
+						uploaderURL,
+						expandedURL,
+						matched,
+						shortURL,
+						oldExpandedURL,
+						regExp = /(http(s?):\/\/(attachments-cdn\.shimo\.im)\/([a-z0-9]{16,})(\/[\S]+)*)(\?)*/i;
 					uploaderURL = each.get('uploaderURL');
-
+					shortURL = each.get('shortURL');
+					oldExpandedURL = each.get('expandedURL');
 					if (uploaderURL) {
-						uploaderURL = uploaderURL.replace('https', 'http');
-						// console.log(uploaderURL);
-						axios({
-							method: 'get',
-							url: uploaderURL,
-							maxRedirects: 0,
-						}).catch(function(error) {
-							expandedURL = error.response.headers.location;
-							expandedURL = decodeURIComponent(expandedURL);
-							if (
-								expandedURL.match(
-									/(attachments-cdn\.shimo\.im)\/([a-z0-9]{16,})\/([\S]+)\?/i, //去掉参数部分
-								)
-							) {
-								attachmentURL = expandedURL.split('?')[0];
-							}
-							if (attachmentURL) {
-								console.log(attachmentURL);
-							}
-						});
+						expandedURL = await expand(uploaderURL);
+
+						expandedURL = decodeURIComponent(expandedURL);
+						matched = expandedURL.match(regExp);
+						if (matched) {
+							attachmentURL = matched[1];
+							console.log(attachmentURL);
+							each.set('attachmentURL', attachmentURL);
+							each.save();
+						}
+
+						// axios({
+						// 	method: 'get',
+						// 	url: uploaderURL,
+						// 	maxRedirects: 0,
+						// }).catch(function(error) {
+						// 	expandedURL = error.response.headers.location;
+						// 	expandedURL = decodeURIComponent(expandedURL);
+						// 	console.log(expandedURL);
+						// 	if (
+						// 		expandedURL.match(
+						// 			regExp, //去掉参数部分
+						// 		)
+						// 	) {
+						// 		attachmentURL = expandedURL.split('?')[0];
+						// 	}
+						// 	if (attachmentURL) {
+						// 		console.log(attachmentURL);
+						// 	}
+						// });
+					} else if (shortURL) {
+						// expandedURL = await expand(shortURL);
+						// console.log(expandedURL);
+					}
+					if (oldExpandedURL) {
+						// console.log(oldExpandedURL);
+						matched = oldExpandedURL.match(regExp);
+						if (matched) {
+							attachmentURL = matched[1];
+							console.log(attachmentURL);
+							each.set('attachmentURL', attachmentURL);
+							each.save();
+						}
 					}
 				});
 		})
