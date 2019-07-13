ffdcb8034f6547f6536b4e1ea7a1cf52b3589768
lcolok(JUB) <425311101@qq.com>
2019-07-13 21:14:36 +0800

    修复调控台显示问题

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 0,
-		minor: 168,
-		build: 25,
+		minor: 169,
+		build: 1,
 		revision: 0
 	},
-	whole: "0.168.25(360)",
-	commits: 360
+	whole: "0.169.1(361)",
+	commits: 361
 }
