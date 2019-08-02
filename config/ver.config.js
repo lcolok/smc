e298ecf467ac42123042b6580a705227f9e417b8
lcolok(JUB) <425311101@qq.com>
2019-08-02 20:49:01 +0800

    修正首页布局

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 1,
-		minor: 44,
-		build: 4,
+		minor: 45,
+		build: 50,
 		revision: 0
 	},
-	whole: "1.44.4(439)",
-	commits: 439
+	whole: "1.45.50(440)",
+	commits: 440
 }
--- src/views/root/Home.vue
+++ src/views/root/Home.vue
@@ -1,142 +1,137 @@
 <template>
 	<v-container fluid grid-list-md fill-height>
-		<v-layout wrap align-space-around justify-center column fill-height>
-			<v-flex>
+		<v-layout wrap align-space-around justify-center row fill-height>
+			<v-flex v-if="$vuetify.breakpoint.smAndDown">
 				<v-card>
-					<v-flex mt-1>
-						<base-searchbar xs12 />
-					</v-flex>
+					<base-searchbar class="pa-3" />
 				</v-card>
 			</v-flex>
-
 			<v-flex xs12>
 				<material-card color="white" :dark="false">
-					<v-card-text>
-						<v-layout row wrap>
-							<v-flex md6 sm12>
-								<h2 class="title font-weight-light">
-									{{ $t('Recently uploaded') }}
-								</h2>
-								<v-container fill-height fluid grid-list-md>
-									<v-layout wrap>
-										<v-flex
-											v-for="(item, index) in results"
-											:key="index"
-											sm6
-											xs12
-											md12
-											lg6
-										>
-											<material-results-card
-												v-bind="item"
-												:title="item.name"
-												:subTitle="item.name_trans"
-												:index="index"
-											/>
-										</v-flex>
-									</v-layout>
-								</v-container>
+					<v-layout row wrap>
+						<v-flex md6 sm12>
+							<h2 class="title font-weight-light">
+								{{ $t('Recently uploaded') }}
+							</h2>
+							<v-container fill-height fluid grid-list-md>
+								<v-layout wrap>
+									<v-flex
+										v-for="(item, index) in results"
+										:key="index"
+										sm6
+										xs12
+										md12
+										lg6
+									>
+										<material-results-card
+											v-bind="item"
+											:title="item.name"
+											:subTitle="item.name_trans"
+											:index="index"
+										/>
+									</v-flex>
+								</v-layout>
+							</v-container>
+						</v-flex>
+
+						<v-flex md6 sm12>
+							<h2 class="title font-weight-light mb-3">
+								Notifications Style
+							</h2>
+
+							<material-notification class="mb-3" color="info"
+								>This is a plain notification</material-notification
+							>
+							<material-notification class="mb-3" color="info" dismissible
+								>This is a notification with close
+								button.</material-notification
+							>
+							<material-notification
+								class="mb-3"
+								color="info"
+								dismissible
+								icon="mdi-bell-plus"
+								>This is a notification with close button and
+								icon.</material-notification
+							>
+							<material-notification
+								class="mb-3"
+								color="info"
+								dismissible
+								icon="mdi-bell-plus"
+							>
+								This is a notification with close button and icon and have many
+								lines. You can see that the icon and the close button are always
+								vertically aligned. This is a beautiful notification. So you
+								don't have to worry about the style.
+							</material-notification>
+						</v-flex>
+					</v-layout>
+					<div class="text-xs-center">
+						<h2 class="title font-weight-light mb-2">Notification Places</h2>
+						<span class="subheading font-weight-light grey--text"
+							>Click to view notifications</span
+						>
+					</div>
+
+					<v-container grid-list-lg>
+						<v-layout justify-center row wrap>
+							<v-flex xs12 sm4>
+								<v-btn block color="success" @click="snack('top', 'left')"
+									>Top Left</v-btn
+								>
 							</v-flex>
-
-							<v-flex md6 sm12>
-								<h2 class="title font-weight-light mb-3">
-									Notifications Style
-								</h2>
-
-								<material-notification class="mb-3" color="info"
-									>This is a plain notification</material-notification
+							<v-flex xs12 sm4>
+								<v-btn block color="success" @click="snack('top')"
+									>Top Center</v-btn
 								>
-								<material-notification class="mb-3" color="info" dismissible
-									>This is a notification with close
-									button.</material-notification
-								>
-								<material-notification
-									class="mb-3"
-									color="info"
-									dismissible
-									icon="mdi-bell-plus"
-									>This is a notification with close button and
-									icon.</material-notification
-								>
-								<material-notification
-									class="mb-3"
-									color="info"
-									dismissible
-									icon="mdi-bell-plus"
-								>
-									This is a notification with close button and icon and have
-									many lines. You can see that the icon and the close button are
-									always vertically aligned. This is a beautiful notification.
-									So you don't have to worry about the style.
-								</material-notification>
 							</v-flex>
 						</v-layout>
-						<div class="text-xs-center">
-							<h2 class="title font-weight-light mb-2">Notification Places</h2>
-							<span class="subheading font-weight-light grey--text"
-								>Click to view notifications</span
-							>
+						<v-layout justify-center row wrap>
+							<v-flex xs12 sm4>
+								<v-btn block color="success" @click="snack('top', 'right')"
+									>Top Right</v-btn
+								>
+							</v-flex>
+							<v-flex xs12 sm4>
+								<v-btn block color="success" @click="snack('bottom', 'left')"
+									>Bottom Left</v-btn
+								>
+							</v-flex>
+						</v-layout>
+						<v-layout justify-center row wrap>
+							<v-flex xs12 sm4>
+								<v-btn block color="success" @click="snack('bottom')"
+									>Bottom Center</v-btn
+								>
+							</v-flex>
+							<v-flex xs12 sm4>
+								<v-btn block color="success" @click="snack('bottom', 'right')"
+									>Bottom Right</v-btn
+								>
+							</v-flex>
+						</v-layout>
+					</v-container>
+
+					<v-snackbar
+						:color="color"
+						:bottom="bottom"
+						:top="top"
+						:left="left"
+						:right="right"
+						v-model="snackbar"
+						dark
+					>
+						<v-icon color="white" class="mr-3">mdi-bell-plus</v-icon>
+						<div>
+							Welcome to
+							<b>Vue Material Dashboard</b> - a beautiful freebie for every web
+							developer.
 						</div>
-
-						<v-container grid-list-lg>
-							<v-layout justify-center row wrap>
-								<v-flex xs12 sm4>
-									<v-btn block color="success" @click="snack('top', 'left')"
-										>Top Left</v-btn
-									>
-								</v-flex>
-								<v-flex xs12 sm4>
-									<v-btn block color="success" @click="snack('top')"
-										>Top Center</v-btn
-									>
-								</v-flex>
-							</v-layout>
-							<v-layout justify-center row wrap>
-								<v-flex xs12 sm4>
-									<v-btn block color="success" @click="snack('top', 'right')"
-										>Top Right</v-btn
-									>
-								</v-flex>
-								<v-flex xs12 sm4>
-									<v-btn block color="success" @click="snack('bottom', 'left')"
-										>Bottom Left</v-btn
-									>
-								</v-flex>
-							</v-layout>
-							<v-layout justify-center row wrap>
-								<v-flex xs12 sm4>
-									<v-btn block color="success" @click="snack('bottom')"
-										>Bottom Center</v-btn
-									>
-								</v-flex>
-								<v-flex xs12 sm4>
-									<v-btn block color="success" @click="snack('bottom', 'right')"
-										>Bottom Right</v-btn
-									>
-								</v-flex>
-							</v-layout>
-						</v-container>
-
-						<v-snackbar
-							:color="color"
-							:bottom="bottom"
-							:top="top"
-							:left="left"
-							:right="right"
-							v-model="snackbar"
-							dark
+						<v-icon size="16" @click="snackbar = false"
+							>mdi-close-circle</v-icon
 						>
-							<v-icon color="white" class="mr-3">mdi-bell-plus</v-icon>
-							<div>
-								Welcome to
-								<b>Vue Material Dashboard</b> - a beautiful freebie for every
-								web developer.
-							</div>
-							<v-icon size="16" @click="snackbar = false"
-								>mdi-close-circle</v-icon
-							>
-						</v-snackbar>
-					</v-card-text>
+					</v-snackbar>
 				</material-card>
 			</v-flex>
 		</v-layout>
