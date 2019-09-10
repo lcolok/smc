6e373f490cf28f42cc6c005fa8a6d5543ab65495
lcolok(JUB) <425311101@qq.com>
2019-09-10 16:24:08 +0800

    去掉 browser-sync 包

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 1,
-		minor: 66,
-		build: 4,
+		minor: 67,
+		build: 7,
 		revision: 0
 	},
-	whole: "1.66.4(468)",
-	commits: 468
+	whole: "1.67.7(469)",
+	commits: 469
 }
--- gulpfile.js/all.js
+++ gulpfile.js/all.js
@@ -9,37 +9,12 @@ var gutil = require('gulp-util');
 var exec = require('child_process').exec;
 var nodemon = require('gulp-nodemon');
 
-
-var browserSync = require('browser-sync').create();
-
-gulp.task('serve', function () {
-    browserSync.init({
-        proxy: 'http://localhost:3000',
-        browser: 'chrome',
-        port: 7000
-    });
-
-    gulp.watch('public/**/*.+(scss|jade|ls)')
-        .on('change', browserSync.reload);
+gulp.task('docs', function(callback) {
+	var docsPath = 'docs/';
+	del([docsPath + '*'], callback);
+	return gulp.src('dist/**/*').pipe(gulp.dest(docsPath));
 });
 
-
-
-gulp.task('docs', function (callback) {
-    var docsPath = 'docs/';
-    del([
-        docsPath + '*',
-    ], callback);
-    return gulp.src('dist/**/*')
-        .pipe(gulp.dest(docsPath))
+gulp.task('getProcessEnv', function(done) {
+	done();
 });
-
-
-
-gulp.task('getProcessEnv', function (done) {
-    done();
-});
-
-
-
-
