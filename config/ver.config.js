30db50a06890d83862513aed1e019df3ea085435
lcolok(JUB) <425311101@qq.com>
2019-09-24 17:55:35 +0800

    加入公众号接口安全域名

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 1,
-		minor: 67,
-		build: 7,
+		minor: 68,
+		build: 14,
 		revision: 0
 	},
-	whole: "1.67.7(469)",
-	commits: 469
+	whole: "1.68.14(470)",
+	commits: 470
 }
