f83d267e53072e7867d4c530b3cdfe80127de234
lcolok(Home) <425311101@qq.com>
2019-10-27 01:40:00 +0800

    Revert "运行就能复制到剪贴板"
    This reverts commit 07e595c35158fec510a7bc3cc67dd2364e6fac28.

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 1,
-		minor: 73,
-		build: 0,
+		minor: 72,
+		build: 10,
 		revision: 0
 	},
-	whole: "1.73.0(475)",
-	commits: 475
+	whole: "1.72.10(474)",
+	commits: 474
 }
