364f215eddbbd1be8dd1d2567d29919b2e735ca3
lcolok(JUB) <425311101@qq.com>
2020-03-07 14:29:12 +0800

    增加 win mac 识别

--- config/ver.config.js
+++ config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 1,
-		minor: 83,
-		build: 1,
+		minor: 84,
+		build: 15,
 		revision: 0
 	},
-	whole: "1.83.1(492)",
-	commits: 492
+	whole: "1.84.15(497)",
+	commits: 497
 }
