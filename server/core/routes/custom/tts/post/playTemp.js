bb4a83009a7e9070b792fcd37e720b3c479d186a
lcolok(JUB) <425311101@qq.com>
2020-03-16 05:14:42 +0800

    Delete playTemp.js

--- server/core/routes/custom/tts/post/playTemp.js
+++
@@ -1,4 +0,0 @@
-const ttsPlayTemp = require('../../../../../../api/xunfei/tts_playTemp');
-const apiAdapter = require('../../../../../../tools/apiAdapter');
-
-module.exports = (...args) => apiAdapter.post(...args, ttsPlayTemp);
