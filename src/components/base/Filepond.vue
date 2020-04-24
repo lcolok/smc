16e19b90a2c8631bc748f4986dd651fd2b75be06
lcolok(JUB) <425311101@qq.com>
2020-04-25 02:17:42 +0800

    filepond自动续传

--- src/components/base/Filepond.vue
+++ src/components/base/Filepond.vue
@@ -68,92 +68,101 @@ export default {
 			},
 			serverOptions: {
 				server: {
-					process: async (
-						fieldName,
-						file,
-						metadata,
-						load,
-						error,
-						progress,
-						abort,
-					) => {
+					process: function(...args) {
+						uploadProgress(...args);
 						// let filename = 'smc' + extname(file.name);
-						let QJ = await AV.Cloud.run('getQiniuJSON', {
-							fileNameArr: [file.name],
-						});
+						function uploadProgress(...args) {
+							const [
+								fieldName,
+								file,
+								metadata,
+								load,
+								error,
+								progress,
+								abort,
+							] = args;
+							AV.Cloud.run('getQiniuJSON', {
+								fileNameArr: [file.name],
+							}).then(QJ => {
+								console.log(QJ);
 
-						console.log(QJ);
+								let observer = {
+									next(res) {
+										let e = res.total;
+										// console.log(e);
+										progress(true, e.loaded, e.size);
+									},
+									error(err) {
+										console.error(err);
+										if (err.code == 401) {
+											uploadProgress(...args);
+										} else {
+											error(err);
+										}
+									},
+									complete(res) {
+										console.log(res);
+										if (res.code == 0) {
+											//原石墨床上传后的登记处理
+											// let json = res;
+											// // console.log(json);
+											// let filename = file.name;
 
-						let observer = {
-							next(res) {
-								let e = res.total;
-								// console.log(e);
-								progress(true, e.loaded, e.size);
-							},
-							error(err) {
-								console.error(err);
-								error(err);
-							},
-							complete(res) {
-								console.log(res);
-								if (res.code == 0) {
-									//原石墨床上传后的登记处理
-									// let json = res;
-									// // console.log(json);
-									// let filename = file.name;
+											// let arr = filename.split('.');
+											// let suffix = arr.pop();
+											// let realName = arr.join('.');
 
-									// let arr = filename.split('.');
-									// let suffix = arr.pop();
-									// let realName = arr.join('.');
+											// json.data.suffix = suffix;
+											// json.data.name = realName;
 
-									// json.data.suffix = suffix;
-									// json.data.name = realName;
+											// AV.Cloud.run('updateShimo', json);
 
-									// AV.Cloud.run('updateShimo', json);
+											//完成后应调用load方法，并传递返回的服务器文件id
+											//该服务器文件id随后在恢复或恢复文件时使用
+											//这样，您的服务器就知道返回哪个文件，而无需向客户端公开该信息
 
-									//完成后应调用load方法，并传递返回的服务器文件id
-									//该服务器文件id随后在恢复或恢复文件时使用
-									//这样，您的服务器就知道返回哪个文件，而无需向客户端公开该信息
+											file.qiniu = res;
+											load(res); //完成后，应该用文件对象或blob调用load方法 load方法接受字符串(id)或对象
+										}
+									},
+								};
 
-									file.qiniu = res;
-									load(res); //完成后，应该用文件对象或blob调用load方法 load方法接受字符串(id)或对象
-								}
-							},
-						};
+								let putExtra = {
+									fname: '', //fname: string，文件原文件名
+									params: {}, //params: object，用来放置自定义变量
+									mimeType: null, //mimeType: null || array，用来限制上传文件类型，为 null 时表示不对文件类型限制；限制类型放到数组里：["image/png", "image/jpeg", "image/gif"]
+								};
 
-						let putExtra = {
-							fname: '', //fname: string，文件原文件名
-							params: {}, //params: object，用来放置自定义变量
-							mimeType: null, //mimeType: null || array，用来限制上传文件类型，为 null 时表示不对文件类型限制；限制类型放到数组里：["image/png", "image/jpeg", "image/gif"]
-						};
+								let config = {
+									// useCdnDomain: true,//表示是否使用 cdn 加速域名，为布尔值，true 表示使用，默认为 false。（感觉开启了之后，上传的速度更慢了）
+									concurrentRequestLimit: 9, //分片上传的并发请求量，number，默认为3；因为浏览器本身也会限制最大并发量，所以最大并发量与浏览器有关。
+								};
 
-						let config = {
-							// useCdnDomain: true,//表示是否使用 cdn 加速域名，为布尔值，true 表示使用，默认为 false。（感觉开启了之后，上传的速度更慢了）
-							concurrentRequestLimit: 9, //分片上传的并发请求量，number，默认为3；因为浏览器本身也会限制最大并发量，所以最大并发量与浏览器有关。
-						};
+								let observable = qiniu.upload(
+									file,
+									QJ[0].key,
+									QJ[0].token,
+									putExtra,
+									config,
+								);
+								let subscription = observable.subscribe(observer); // 上传开始
+								// or
+								//   let subscription = observable.subscribe(next, error, complete); // 这样传参形式也可以
 
-						let observable = qiniu.upload(
-							file,
-							QJ[0].key,
-							QJ[0].token,
-							putExtra,
-							config,
-						);
-						let subscription = observable.subscribe(observer); // 上传开始
-						// or
-						//   let subscription = observable.subscribe(next, error, complete); // 这样传参形式也可以
+								// Should expose an abort method so the request can be cancelled
+								//应该公开一个中止方法，以便取消请求
 
-						// Should expose an abort method so the request can be cancelled
-						//应该公开一个中止方法，以便取消请求
-						return {
-							abort: () => {
-								// 如果用户点击了取消按钮，则进入该功能
-								subscription.unsubscribe(); // 上传取消
+								return {
+									abort: () => {
+										//让filepond知道请求已被取消
+										abort();
 
-								//让filepond知道请求已被取消
-								abort();
-							},
-						};
+										// 如果用户点击了取消按钮，则进入该功能
+										subscription.unsubscribe(); // 上传取消
+									},
+								};
+							});
+						}
 					},
 				},
 			},
