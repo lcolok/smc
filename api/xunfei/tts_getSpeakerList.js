11e393a4d7bd7e5db8857d346d7fa070880ae87b
lcolok(JUB) <425311101@qq.com>
2020-04-27 23:17:30 +0800

    应急用一下

--- api/xunfei/tts_getSpeakerList.js
+++ api/xunfei/tts_getSpeakerList.js
@@ -3,25 +3,16 @@ const { getSpeakerList } = require('./core');
 const AV = require('leancloud-storage');
 
 module.exports = async function() {
-	// const speakersArr = await getSpeakerList();
+	const speakersArr = await getSpeakerList();
 
-	// extract = speakersArr.map(e =>
-	// 	_.pick(e, [
-	// 		'speaker_name',
-	// 		'speaker_no',
-	// 		'speaker_language',
-	// 		'speaker_specialty',
-	// 		'audio_url',
-	// 		'img_url',
-	// 		'used_times',
-	// 	]),
-	// );
-	const query = new AV.Query('tts_SpeakerList');
-	query.exists('speakersArr');
-	query.descending('createdAt');
-	query.limit(1);
-	const item = await query.first();
-	const speakersArr = item.get('speakersArr');
+	// const query = new AV.Query('tts_SpeakerList');
+	// query.exists('speakersArr');
+	// query.descending('createdAt');
+	// query.limit(1);
+	// const item = await query.first();
+	// console.log(item);
+	// const speakersArr = item.get('speakersArr');
+
 	tagsArr = ['中文主播', '英语主播', '方言主播', '特色主播'];
 	extract = [];
 	tagsArr.map(k => {
